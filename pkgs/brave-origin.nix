532e3e4795b34c33143b9f2c0ad800b8373086d9
nixos-upd <nixos-upd@localhost>
2026-08-13 12:58:55 -0600

    auto: actualizacion preparada

--- pkgs/brave-origin.nix
+++ pkgs/brave-origin.nix
@@ -63,7 +63,7 @@
 # does. Bump `version` + `hash` together; both come from
 # https://brave-browser-apt-release.s3.brave.com/dists/stable/main/binary-amd64/Packages
 let
-  version = "1.93.134";
+  version = "1.93.136";
 
   deps = [
     alsa-lib
@@ -162,7 +162,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://brave-browser-apt-release.s3.brave.com/pool/main/b/brave-origin/brave-origin_${version}_amd64.deb";
-    hash = "sha256-KIuPPIdbzYVd/REn/YVZ1+CVIid8/VaeP7LUThBuxTI=";
+    hash = "sha256-rg5XjxusjUKl4YzTpX1zIVmhv71YwV6qgVprrW/Gklo=";
   };
 
   dontConfigure = true;
